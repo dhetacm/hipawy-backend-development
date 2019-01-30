e1c15bbc413b0273845766f0fdbfb7d2897a5973
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-01-30 11:28:25 +0700

    Create auth and user route and controller

--- server.js
+++ server.js
@@ -9,4 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/users", require("./routes/users"));
+app.use("/auth", require("./routes/auth"));
+
 app.listen(PORT, () => console.log(`Server start in PORT ${PORT}`));
