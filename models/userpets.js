0bd75497feebb30b8d4a0884691a5e7803552f29
Dheta Catur Maharani <theresiadhetacm@gmail.com>
2019-02-01 15:14:45 +0700

    Add table userpets association

--- models/userpets.js
+++ models/userpets.js
@@ -1,10 +1,23 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const userPets = sequelize.define('userPets', {
-    status: DataTypes.STRING
-  }, {});
+  const userPets = sequelize.define(
+    "userPets",
+    {
+      status: DataTypes.STRING
+    },
+    {}
+  );
   userPets.associate = function(models) {
-    // associations can be defined here
+    models.userPets.belongsTo(models.user, {
+      onDelete: "CASCADE",
+      foreignKey: { name: id_user, allowNull: false },
+      targetKey: "id"
+    });
+    models.userPets.belongsTo(models.pets, {
+      onDelete: "CASCADE",
+      foreignKey: { name: id_pet, allowNull: false },
+      targetKey: "id"
+    });
   };
   return userPets;
 };
